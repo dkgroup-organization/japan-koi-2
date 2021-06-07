1beca6cfaf40e0561e9fde5970c05f4709fc1b30
abdelghani-khalidi <51909290+abdelghani-khalidi@users.noreply.github.com>
2021-06-07 11:02:48 +0100

    Add files via upload

---
+++ all_in_one_cancel/models/__init__.py
@@ -0,0 +1,6 @@
+from . import purchase_order
+from . import res_config_settings
+from . import company
+from . import sale_order
+from . import warehouse
+from . import stock_picking
---
+++ all_in_one_cancel/models/company.py
@@ -0,0 +1,7 @@
+from odoo import api, fields, models
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    cancel_delivery_order_for_po = fields.Boolean(string="Cancel Delivery Order?")
+    cancel_invoice_for_po = fields.Boolean(string='Cancel Invoice?')
+    cancel_done_picking = fields.Boolean(string='Cancel Done Delivery?')
